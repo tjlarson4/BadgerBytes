65ded9e4ae439190ebda429b1125eadfefcd6193
Thor <tjlarson4@wisc.edu>
2021-02-17 23:41:24 -0600

    Initial Commit

---
+++ BadgerBytes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BadgerBytes
+//
+//  Created by Thor Larson on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
